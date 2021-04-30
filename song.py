582c8d48dbbea713482c0ef89e6941f15d5a641d
PIUphil <58851945+PIUphil@users.noreply.github.com>
2021-04-30 14:49:11 +0900

    Create song.py

---
+++ song.py
@@ -0,0 +1,175 @@
+from pop import PiezoBuzzer
+
+
+# def main():
+#     pass
+
+
+def piezo_buzzer():
+    buzzer = PiezoBuzzer()
+
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,1,4) #도
+    # buzzer.tone(4,3,4) #레
+    # buzzer.tone(4,5,4) #미
+    # buzzer.tone(4,6,4) #파
+    # buzzer.tone(4,8,4) #솔
+    # buzzer.tone(4,10,4) #라
+    # buzzer.tone(4,12,4) #시
+    # buzzer.tone(5,1,4) #도
+
+# 떳다떳다 비행기
+    # buzzer.setTempo(180)
+    # buzzer.tone(4,5,4) #미
+    # buzzer.setTempo(60)
+    # buzzer.tone(4,3,4) #레
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,1,4) #도
+    # buzzer.tone(4,3,4) #레
+    # buzzer.tone(4,5,4) #미
+    # buzzer.tone(4,5,4) #미
+    # buzzer.setTempo(240)
+    # buzzer.tone(4,5,4) #미
+
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,5,4) #레
+    # buzzer.tone(4,5,4) #레
+    # buzzer.setTempo(240)
+    # buzzer.tone(4,5,4) #레
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,5,4) #미
+    # buzzer.tone(4,5,4) #솔
+    # buzzer.setTempo(240)
+    # buzzer.tone(4,5,4) #솔
+
+
+    # buzzer.setTempo(180)
+    # buzzer.tone(4,5,4) #미
+    # buzzer.setTempo(60)
+    # buzzer.tone(4,3,4) #레
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,1,4) #도
+    # buzzer.tone(4,3,4) #레
+    # buzzer.tone(4,5,4) #미
+    # buzzer.tone(4,5,4) #미
+    # buzzer.setTempo(240)
+    # buzzer.tone(4,5,4) #미
+
+    # buzzer.setTempo(120)
+    # buzzer.tone(4,3,4) #레
+    # buzzer.tone(4,3,4) #레
+    # buzzer.setTempo(180)
+    # buzzer.tone(4,5,4) #미
+    # buzzer.setTempo(60)
+    # buzzer.tone(4,3,4) #레
+    # buzzer.setTempo(360)
+    # buzzer.tone(4,1,4) #도
+
+
+# 무엇이 무엇이 똑같을까
+    # buzzer.setTempo(120)
+    # buzzer.tone(5,1,4) #도
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,8,4) #솔
+    # buzzer.tone(5,1,4) #도
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,8,4) #솔
+    # buzzer.tone(5,10,4) #라
+    # buzzer.tone(5,10,4) #라
+    # buzzer.tone(5,10,4) #라
+    # buzzer.setTempo(360)
+    # buzzer.tone(5,8,4) #솔
+
+    # buzzer.setTempo(120)
+    # buzzer.tone(5,6,4) #파
+    # buzzer.tone(5,6,4) #파
+    # buzzer.tone(5,6,4) #파
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,3,4) #레
+    # buzzer.tone(5,3,4) #레
+    # buzzer.tone(5,3,4) #레
+    # buzzer.setTempo(360)
+    # buzzer.tone(5,1,4) #도
+    
+
+# # 도레미
+    # buzzer.tone(5,1,3) #도
+    # buzzer.tone(5,3,6) #레
+    # buzzer.tone(5,5,3) #미
+    # buzzer.tone(5,1,6) #도
+    # buzzer.tone(5,5,4) #미
+    # buzzer.tone(5,1,3) #도
+    # buzzer.tone(5,5,4) #미
+
+
+#piezo_buzzer()
+
+# def piezo_buzzer_unittest():
+#     p = PiezoBuzzer()
+
+#     p.setTempo(120)
+#     p.tone(4, 8, 4)
+#     p.tone(4, 8, 4)
+#     p.tone(4, 10, 4)
+#     p.tone(4, 10, 4)
+#     p.tone(4, 8, 4)
+#     p.tone(4, 8, 4)
+#     p.tone(4, 5, 2)
+
+#     p.tone(4, 8, 4)
+#     p.tone(4, 5, 4)
+#     p.tone(4, 3, 4)
+#     p.tone(4, 5, 4)
+#     p.tone(4, 1, 1)
+
+# buzzer.setTempo(80)
+# 미라도^시라미^레^시  
+    buzzer.tone(4,5,6)#4
+    buzzer.tone(4,10,4)#6 
+    buzzer.tone(5,1,10)#0
+    buzzer.tone(4,12,6)#4
+    buzzer.tone(4,10,3)#7
+
+    buzzer.tone(5,5,4)#6
+    buzzer.tone(5,3,2)# 8
+    buzzer.tone(4,12,2)#8
+
+# 라도^시솔라미
+    buzzer.tone(4,10,4)
+    buzzer.tone(5,1,12)
+    buzzer.tone(4,12,7)
+    buzzer.tone(4,8,3)
+    buzzer.tone(4,10,4)
+    buzzer.tone(4,5,2) 
+
+# 미라도^시라 / 미^솔^파#^파^
+    buzzer.tone(4,5,6)
+    buzzer.tone(4,10,4) 
+    buzzer.tone(5,1,10)
+    buzzer.tone(4,12,6)
+    buzzer.tone(4,10,3)
+
+    buzzer.tone(5,5,7)
+    buzzer.tone(5,8,5)
+    buzzer.tone(5,7,4)
+    buzzer.tone(5,6,3)
+
+# 레^파^미^레^시도^라
+    buzzer.tone(5,3,3)# 8
+    buzzer.tone(5,6,5)# 8
+    buzzer.tone(5,5,7)# 8
+    buzzer.tone(5,3,6)# 8
+
+    buzzer.tone(4,12,3)# 8
+    buzzer.tone(5,1,3)# 8
+    buzzer.tone(4,10,2)# 8
+
+
+
+
+
+if __name__=="__main__":
+    # piezo_buzzer_unittest()
+    piezo_buzzer()
