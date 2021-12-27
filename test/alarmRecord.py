2540fc95071fb1cf5d064a9d5bd003f9704a28ab
PIUphil <ahnkp08@daum.net>
2021-12-27 22:33:21 +0900

    Update alarmRecord.py

--- test/alarmRecord.py
+++ test/alarmRecord.py
@@ -9,8 +9,7 @@ newAlarm = np.array([])
 ll = np.array(list("a"+str(i) for i in reversed(range(147))))
 
 for line in ll:
-    newAlarm = np.append(newAlarm, now + " " + line)
-#print(newAlarm)
+    newAlarm = np.append(newAlarm, now + " " + line+"\n")
 
 
 # 기존 기록 가져오기
@@ -18,7 +17,6 @@ oldAlarm = np.array([])
 with open('tt.csv', 'r+') as f:
     for line in f.readlines():
         oldAlarm = np.append(oldAlarm, line)
-# print(oldAlarm)
 
 
 # 새 기록 뒤에 기존 기록 붙이기
@@ -27,11 +25,14 @@ alarm = np.append(newAlarm, oldAlarm)
 if len(alarm)>50:
     alarm = alarm[:50]
 
+# 가장 마지막줄은 줄넘김 삭제 (마지막줄 "\n" 지우기)
+if alarm[-1][-1:] == "\n":
+    alarm[-1] = alarm[-1][:-1]
 
 # 파일에 알람기록 덮어쓰기
 with open('tt.csv', 'w') as f:
     for i in alarm:
-        f.write(i+"\n")
+        f.write(i)
 
 print(len(alarm))
 # print(alarm)
